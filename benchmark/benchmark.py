978c0e686d1607767c0927878f29919a76eb3af5
Daniel Carl Jones <djonesoa@amazon.co.uk>
2024-08-27 17:20:16 +0100

    Move FIO job spec to FIO-specific directory, move FIO output to path including FIO job name
    This is general prep for having multiple FIO job specs.
    
    Signed-off-by: Daniel Carl Jones <djonesoa@amazon.co.uk>

--- benchmark/benchmark.py
+++ benchmark/benchmark.py
@@ -3,6 +3,7 @@ import dataclasses
 import json
 import logging
 import os
+from os import path
 import subprocess
 import tempfile
 from typing import Optional
@@ -86,21 +87,24 @@ def _run_fio(cfg: DictConfig, mount_dir: str) -> None:
     Run the FIO workload against the file system.
     """
     FIO_BINARY = "/usr/bin/fio"
-    subprocess_args = [
-        FIO_BINARY,
-        "--output=fio-output.json",
-        "--output-format=json",
-        "--eta=never",
-        f"--directory={mount_dir}",
-        hydra.utils.to_absolute_path("sequential_read.fio"),
-    ]
-    subprocess_env = {
-        "NUMJOBS": str(cfg['application_workers']),
-        "SIZE_GIB": str(100),
-        "DIRECT": str(1 if cfg['direct_io'] else 0),
-    }
-    log.debug(f"Running FIO with args: %s; env: %s", subprocess_args, subprocess_env)
-    subprocess.check_output(subprocess_args, env=subprocess_env)
+    job_names = ["sequential_read"]
+    for job_name in job_names:
+        job_out_dir = f"fio_out/{job_name}/"
+        subprocess_args = [
+            FIO_BINARY,
+            f"--output={path.join(job_out_dir, "result.json")}",
+            "--output-format=json",
+            "--eta=never",
+            f"--directory={mount_dir}",
+            hydra.utils.to_absolute_path(f"fio/{job_name}.fio"),
+        ]
+        subprocess_env = {
+            "NUMJOBS": str(cfg['application_workers']),
+            "SIZE_GIB": str(100),
+            "DIRECT": str(1 if cfg['direct_io'] else 0),
+        }
+        log.debug(f"Running FIO with args: %s; env: %s", subprocess_args, subprocess_env)
+        subprocess.check_output(subprocess_args, env=subprocess_env)
 
 def _unmount_mp(mount_dir: str) -> None:
     """
